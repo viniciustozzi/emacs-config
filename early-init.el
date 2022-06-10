28d534d8bec22f7acddda80990231eb6bdaf3532
Vinicius Tozzi <viniciustozzi@pm.me>
2022-06-10 22:24:23 +0200

    initial commit

---
+++ early-init.el
@@ -0,0 +1,38 @@
+;; Increase the GC threshold for faster startup
+;; The default is 800 kilobytes.  Measured in bytes.
+(setq gc-cons-threshold (* 50 1000 1000))
+
+(setq package-enable-at-startup nil)
+
+;; Prefer loading newest compiled .el file
+(customize-set-variable 'load-prefer-newer noninteractive)
+
+;; Native compilation settings
+(when (featurep 'native-compile)
+  ;; Silence compiler warnings as they can be pretty disruptive
+  (setq native-comp-async-report-warnings-errors nil)
+
+  ;; Make native compilation happens asynchronously
+  (setq native-comp-deferred-compilation t)
+
+  ;; Set the right directory to store the native compilation cache
+  ;; NOTE the method for setting the eln-cache directory depends on the emacs version
+  (when (fboundp 'startup-redirect-eln-cache)
+    (if (version< emacs-version "29")
+        (add-to-list 'native-comp-eln-load-path (convert-standard-filename (expand-file-name "var/eln-cache/" user-emacs-directory)))
+      (startup-redirect-eln-cache (convert-standard-filename (expand-file-name "var/eln-cache/" user-emacs-directory)))))
+
+  (add-to-list 'native-comp-eln-load-path (expand-file-name "eln-cache/" user-emacs-directory)))
+
+;; Remove some unneeded UI elements (the user can turn back on anything they wish)
+(setq inhibit-startup-message t)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(push '(mouse-color . "white") default-frame-alist)
+
+;; Loads a nice blue theme, avoids the white screen flash on startup.
+(load-theme 'deeper-blue t)
+
+;; Make the initial buffer load faster by setting its mode to fundamental-mode
+(customize-set-variable 'initial-major-mode 'fundamental-mode)
---
+++ init.el
@@ -0,0 +1,419 @@
+;;---------Straight package manager-------
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;;Use-package integration
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+
+;;;--------General Defaults--------
+;;Disable backup files
+(setq make-backup-files nil)
+
+(setq initial-scratch-message "")
+
+;; Revert Dired and other buffers
+(customize-set-variable 'global-auto-revert-non-file-buffers t)
+
+;; Revert buffers when the underlying file has changed
+(global-auto-revert-mode 1)
+
+;; Use "y" and "n" to confirm/negate prompt instead of "yes" and "no"
+;; Using `advice' here to make it easy to reverse in custom
+;; configurations with `(advice-remove 'yes-or-no-p #'y-or-n-p)'
+;;
+;; N.B. Emacs 28 has a variable for using short answers, which should
+;; be preferred if using that version or higher.
+(if (boundp 'use-short-answers)
+    (setq use-short-answers t)
+  (advice-add 'yes-or-no-p :override #'y-or-n-p))
+
+;; Do not saves duplicates in kill-ring
+(customize-set-variable 'kill-do-not-save-duplicates t)
+
+;; Make scrolling less stuttered
+(setq auto-window-vscroll nil)
+(customize-set-variable 'fast-but-imprecise-scrolling t)
+(customize-set-variable 'scroll-conservatively 101)
+(customize-set-variable 'scroll-margin 0)
+(customize-set-variable 'scroll-preserve-screen-position t)
+
+;; Better support for files with long lines
+(setq-default bidi-paragraph-direction 'left-to-right)
+(setq-default bidi-inhibit-bpa t)
+(global-so-long-mode 1)
+
+(add-hook 'after-save-hook #'executable-make-buffer-file-executable-if-script-p)
+
+;;;-------Evil Mode---------
+(straight-use-package 'evil)
+(straight-use-package 'undo-tree)
+(straight-use-package 'evil-collection)
+(straight-use-package 'evil-nerd-commenter)
+
+;; Turn on undo-tree globally
+(global-undo-tree-mode)
+
+;; Set some variables that must be configured before loading the package
+(customize-set-variable 'evil-want-integration t)
+(customize-set-variable 'evil-want-keybinding nil)
+(customize-set-variable 'evil-want-C-i-jump nil)
+(customize-set-variable 'evil-respect-visual-line-mode t)
+(customize-set-variable 'evil-undo-system 'undo-tree)
+
+(setq evil-want-C-u-scroll t)
+
+;; Turn on Evil Nerd Commenter
+(evilnc-default-hotkeys)
+
+;; Make C-g revert to normal state
+(define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+
+;; Rebind `universal-argument' to 'C-M-u' since 'C-u' now scrolls the buffer
+(global-set-key (kbd "C-M-u") 'universal-argument)
+(global-set-key (kbd "C-u") #'evil-scroll-up)
+
+;; C-h is backspace in insert state
+(define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+;; Use visual line motions even outside of visual-line-mode buffers
+(evil-global-set-key 'motion "j" 'evil-next-visual-line)
+(evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+;; Load Evil and enable it globally
+(require 'evil)
+(evil-mode 1)
+
+;; Make sure some modes start in Emacs state
+(dolist (mode '(custom-mode
+                eshell-mode
+                term-mode))
+  (add-to-list 'evil-emacs-state-modes mode))
+ 
+(evil-collection-init)
+
+;;;---------UI Improvements----------
+(setq default-frame-alist '((font . "Fira Mono Medium 18")))
+
+(straight-use-package 'gruber-darker-theme)
+(straight-use-package 'modus-themes)
+(straight-use-package 'all-the-icons)
+(straight-use-package 'doom-modeline)
+(straight-use-package 'elisp-demos)
+(straight-use-package 'helpful)
+
+;; Start up the modeline after initialization is finished
+(add-hook 'after-init-hook 'doom-modeline-init)
+
+(require 'modus-themes)
+(modus-themes-load-themes)
+(modus-themes-load-vivendi)   
+
+;; Configure `doom-modeline'
+(customize-set-variable 'doom-modeline-height 15)
+(customize-set-variable 'doom-modeline-bar-width 6)
+(customize-set-variable 'doom-modeline-minor-modes t)
+(customize-set-variable 'doom-modeline-buffer-file-name-style 'truncate-except-project)
+
+;; Make `describe-*' screens more helpful
+(require 'helpful)
+(define-key helpful-mode-map [remap revert-buffer] #'helpful-update)
+(global-set-key [remap describe-command] #'helpful-command)
+(global-set-key [remap describe-function] #'helpful-callable)
+(global-set-key [remap describe-key] #'helpful-key)
+(global-set-key [remap describe-symbol] #'helpful-symbol)
+(global-set-key [remap describe-variable] #'helpful-variable)
+(global-set-key (kbd "C-h F") #'helpful-function)
+(global-set-key (kbd "C-h K") #'describe-keymap)
+;; also add some examples
+(require 'elisp-demos)
+(advice-add 'helpful-update :after #'elisp-demos-advice-helpful-update)
+
+;; add visual pulse when changing focus, like beacon but built-in
+;; from from https://karthinks.com/software/batteries-included-with-emacs/
+(defun pulse-line (&rest _)
+  "Pulse the current line."
+  (pulse-momentary-highlight-one-line (point)))
+
+(dolist (command '(scroll-up-command scroll-down-command
+                                     recenter-top-bottom other-window))
+  (advice-add command :after #'pulse-line))
+
+;;;-------COMPLETION/SEARCHING------------
+
+(straight-use-package 'cape)
+(straight-use-package 'consult)
+(straight-use-package 'corfu-doc)
+(straight-use-package 'corfu)
+(straight-use-package 'embark)
+(straight-use-package 'embark-consult)
+(straight-use-package 'marginalia)
+(straight-use-package 'orderless)
+(straight-use-package '( vertico :files (:defaults "extensions/*")
+                         :includes (vertico-buffer
+                                    vertico-directory
+                                    vertico-flat
+                                    vertico-indexed
+                                    vertico-mouse
+                                    vertico-quick
+                                    vertico-repeat
+                                    vertico-reverse)))
+
+(require 'vertico)
+
+(defun minibuffer-backward-kill (arg)
+  "When minibuffer is completing a file name delete up to parent
+folder, otherwise delete a word"
+  (interactive "p")
+  (if minibuffer-completing-file-name
+      ;; Borrowed from https://github.com/raxod502/selectrum/issues/498#issuecomment-803283608
+      (if (string-match-p "/." (minibuffer-contents))
+          (zap-up-to-char (- arg) ?/)
+        (delete-minibuffer-contents))
+    (backward-kill-word arg)))
+
+;; Configure directory extension.
+(use-package vertico-directory
+  :after vertico
+  :ensure nil
+  ;; More convenient directory navigation commands
+  :bind (:map vertico-map
+              ("RET" . vertico-directory-enter)
+              ("DEL" . vertico-directory-delete-char)
+              ("M-DEL" . vertico-directory-delete-word))
+  ;; Tidy shadowed file names
+  :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
+
+(with-eval-after-load 'evil
+  (define-key vertico-map (kbd "C-j") 'vertico-next)
+  (define-key vertico-map (kbd "C-k") 'vertico-previous)
+  (define-key vertico-map (kbd "M-h") 'vertico-directory-up))
+
+;; Cycle back to top/bottom result when the edge is reached
+(customize-set-variable 'vertico-cycle t)
+
+;; Start Vertico
+(vertico-mode 1)
+
+;;; Marginalia
+;; Configure Marginalia
+(require 'marginalia)
+(customize-set-variable 'marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light nil))
+(marginalia-mode 1)
+
+;; Set some consult bindings
+(global-set-key (kbd "C-s") 'consult-line)
+(define-key minibuffer-local-map (kbd "C-r") 'consult-history)
+
+(setq completion-in-region-function #'consult-completion-in-region)
+
+;;; Orderless
+;; Set up Orderless for better fuzzy matching
+(require 'orderless)
+(customize-set-variable 'completion-styles '(orderless))
+(customize-set-variable 'completion-category-overrides '((file (styles . (partial-completion)))))
+
+;;; Embark
+(require 'embark)
+(require 'embark-consult)
+
+(global-set-key [remap describe-bindings] #'embark-bindings)
+(global-set-key (kbd "C-.") 'embark-act)
+
+;; Use Embark to show bindings in a key prefix with `C-h`
+(setq prefix-help-command #'embark-prefix-help-command)
+
+(with-eval-after-load 'embark-consult
+  (add-hook 'embark-collect-mode-hook #'consult-preview-at-point-mode))
+
+;;; Corfu
+;; Setup corfu for popup like completion
+(customize-set-variable 'corfu-cycle t) ; Allows cycling through candidates
+(customize-set-variable 'corfu-auto t)  ; Enable auto completion
+(customize-set-variable 'corfu-auto-prefix 2) ; Complete with less prefix keys
+(customize-set-variable 'corfu-auto-delay 0.0) ; No delay for completion
+(customize-set-variable 'corfu-echo-documentation 0.25) ; Echo docs for current completion option
+
+(global-corfu-mode 1)
+
+(add-hook 'corfu-mode-hook #'corfu-doc-mode)
+(define-key corfu-map (kbd "M-p") #'corfu-doc-scroll-down)
+(define-key corfu-map (kbd "M-n") #'corfu-doc-scroll-up)
+(define-key corfu-map (kbd "M-d") #'corfu-doc-toggle)
+
+;;; Cape
+
+;; Setup Cape for better completion-at-point support and more
+(require 'cape)
+
+;; Add useful defaults completion sources from cape
+(add-to-list 'completion-at-point-functions #'cape-file)
+(add-to-list 'completion-at-point-functions #'cape-dabbrev)
+
+;; Silence the pcomplete capf, no errors or messages!
+;; Important for corfu
+(advice-add 'pcomplete-completions-at-point :around #'cape-wrap-silent)
+
+;; Ensure that pcomplete does not write to the buffer
+;; and behaves as a pure `completion-at-point-function'.
+(advice-add 'pcomplete-completions-at-point :around #'cape-wrap-purify)
+(add-hook 'eshell-mode-hook
+          (lambda () (setq-local corfu-quit-at-boundary t
+				 corfu-quit-no-match t
+				 corfu-auto nil)
+            (corfu-mode)))
+
+;;;---------WHICH-KEY----------
+(straight-use-package 'which-key)
+(require 'which-key)
+(setq which-key-idle-delay 0.3)
+(which-key-mode 1)
+
+;;;---------GENERAL KEYBINDINGS------------
+(use-package general
+  :config
+  (general-evil-setup t)
+  (general-create-definer leader-keys
+    :keymaps '(normal)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+
+  (leader-keys
+    "." '(find-file-at-point :which-key "Find File"))
+  (leader-keys
+    "h" '(:ignore t :which-key "Help")
+    "hf" '(describe-function :which-key "Function")
+    "hv" '(describe-function :which-key "Variable")
+    "hk" '(describe-key :which-key "Key"))
+  (leader-keys
+    "b" '(:ignore t :which-key "Buffer")
+    "bk" '(kill-current-buffer :which-key "Kill Buffer"))
+  )
+
+;;;---------LSP----------------
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (clojure-mode . lsp)
+	 (rust-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+(use-package lsp-ui :commands lsp-ui-mode)
+
+;;Syntax checking
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+;;For completion
+(use-package company)
+
+;;For debugging
+(use-package dap-mode)
+
+;;;---------Treemacs-----------
+(use-package treemacs
+  :ensure t
+  :defer t
+  :config
+(progn
+    (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay        0.5
+          treemacs-directory-name-transformer      #'identity
+          treemacs-display-in-side-window          t
+          treemacs-eldoc-display                   'simple
+          treemacs-file-event-delay                5000
+          treemacs-file-extension-regex            treemacs-last-period-regex-value
+          treemacs-file-follow-delay               0.2
+          treemacs-file-name-transformer           #'identity
+          treemacs-follow-after-init               t
+          treemacs-expand-after-init               t
+          treemacs-find-workspace-method           'find-for-file-or-pick-first
+          treemacs-git-command-pipe                ""
+          treemacs-goto-tag-strategy               'refetch-index
+          treemacs-header-scroll-indicators        '(nil . "^^^^^^")
+          treemacs-indentation                     2
+          treemacs-indentation-string              " "
+          treemacs-is-never-other-window           nil
+          treemacs-max-git-entries                 5000
+          treemacs-missing-project-action          'ask
+          treemacs-move-forward-on-expand          nil
+          treemacs-no-png-images                   nil
+          treemacs-no-delete-other-windows         t
+          treemacs-project-follow-cleanup          nil
+          treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                        'left
+          treemacs-read-string-input               'from-child-frame
+          treemacs-recenter-distance               0.1
+          treemacs-recenter-after-file-follow      nil
+          treemacs-recenter-after-tag-follow       nil
+          treemacs-recenter-after-project-jump     'always
+          treemacs-recenter-after-project-expand   'on-distance
+          treemacs-litter-directories              '("/node_modules" "/.venv" "/.cask")
+          treemacs-show-cursor                     nil
+          treemacs-show-hidden-files               t
+          treemacs-silent-filewatch                nil
+          treemacs-silent-refresh                  nil
+          treemacs-sorting                         'alphabetic-asc
+          treemacs-select-when-already-in-treemacs 'move-back
+          treemacs-space-between-root-nodes        t
+          treemacs-tag-follow-cleanup              t
+          treemacs-tag-follow-delay                1.5
+          treemacs-text-scale                      nil
+          treemacs-user-mode-line-format           nil
+          treemacs-user-header-line-format         nil
+          treemacs-wide-toggle-width               70
+          treemacs-width                           35
+          treemacs-width-increment                 1
+          treemacs-width-is-initially-locked       t
+          treemacs-workspace-switch-cleanup        nil)
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode 'always)))
+
+(use-package treemacs-evil
+  :after (treemacs evil)
+  :ensure t)
+
+(use-package treemacs-icons-dired
+  :hook (dired-mode . treemacs-icons-dired-enable-once)
+  :ensure t)
+
+;;;---------RUST---------------
+(use-package rustic)
+
+;;;---------CLOJURE------------
+(straight-use-package 'cider)
+(straight-use-package 'clojure-mode)
+(require 'cider)
+(require 'clojure-mode)
+
+;;;---------CUSTOM------------
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("dad40020beea412623b04507a4c185079bff4dcea20a93d8f8451acb6afc8358" "e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
